3a5054e96f1fb972e428240eaffae58ca274cead
Shubham Sharma <shubham16.0497@gmail.com>
2020-06-16 02:53:07 +0530

    Initial commit - Working game to play and make scores.

---
+++ android/src/com/supermario/coinman/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.supermario.coinman.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.supermario.coinman.game.CoinMan;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CoinMan(), config);
+	}
+}
